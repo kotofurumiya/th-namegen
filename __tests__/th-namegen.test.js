00bc735789078e1caa3446de6693d80d9a570531
Koto Furumiya <kotofurumiya@gmail.com>
2021-03-27 18:33:08 +0900

    test: use uvu rather than jest

--- __tests__/th-namegen.test.js
+++ __tests__/th-namegen.test.js
@@ -1,4 +1,7 @@
-import { generateName } from '../';
+import { test, suite } from 'uvu';
+import * as assert from 'uvu/assert';
+
+import { generateName } from '../build/index.js';
 
 const createFakeRandomFunc = (retValList) => {
   let remains = [...retValList];
@@ -17,45 +20,41 @@ const testCharacters = [
   { nameType: 'fullname', firstName: 'marisa', lastName: 'kirisame' },
 ];
 
-describe('test internals', () => {
-  test('createFakeRandomFunc', () => {
-    const fn = createFakeRandomFunc([111, 222, 333]);
-    const gen = new Array(6).fill(0).map(() => fn(0, 999));
-    expect(gen).toHaveLength(6);
-    expect(gen[0]).toBe(111);
-    expect(gen[1]).toBe(222);
-    expect(gen[2]).toBe(333);
-    expect(gen[3]).toBe(111);
-    expect(gen[4]).toBe(222);
-    expect(gen[5]).toBe(333);
-  });
+const TestInternals = suite('Test Internals');
+const ThNameGenBasics = suite('th-namegen basics');
+
+TestInternals('createFakeRandomFunc', () => {
+  const fn = createFakeRandomFunc([111, 222, 333]);
+  const generated = new Array(6).fill(0).map(() => fn(0, 999));
+  assert.equal(generated, [111, 222, 333, 111, 222, 333]);
 });
 
-describe('th-namegen basic functions', () => {
-  test('generateName()', () => {
-    const randomFunc = createFakeRandomFunc([0, 1]);
-    const generated = generateName({ randomFunc, characterCandidates: testCharacters });
-    expect(generated).toBe('reimu-kirisame');
-  });
-
-  test('generateName() with simple name character', () => {
-    const simpleCharacter = { nameType: 'simple', name: 'mima' };
-    const randomFunc = createFakeRandomFunc([0]);
-    const generated = generateName({ randomFunc, characterCandidates: [simpleCharacter] });
-    expect(generated).toBe('mima-mima');
-  });
-
-  test('generateName() with custom delimiter', () => {
-    const delimiter = '@';
-    const randomFunc = createFakeRandomFunc([0, 1]);
-    const generated = generateName({ randomFunc, delimiter, characterCandidates: testCharacters });
-    expect(generated).toBe('reimu@kirisame');
-  });
-
-  test('generateName() with custom converter', () => {
-    const nameChunkConverter = (name, context) => name[0].toUpperCase() + name.slice(1);
-    const randomFunc = createFakeRandomFunc([0, 1]);
-    const generated = generateName({ randomFunc, characterCandidates: testCharacters, nameChunkConverter });
-    expect(generated).toBe('Reimu-Kirisame');
-  });
+ThNameGenBasics('generateName()', () => {
+  const randomFunc = createFakeRandomFunc([0, 1]);
+  const generated = generateName({ randomFunc, characterCandidates: testCharacters });
+  assert.equal(generated, 'reimu-kirisame');
 });
+
+ThNameGenBasics('generateName() with simple name character', () => {
+  const simpleCharacter = { nameType: 'simple', name: 'mima' };
+  const randomFunc = createFakeRandomFunc([0]);
+  const generated = generateName({ randomFunc, characterCandidates: [simpleCharacter] });
+  assert.equal(generated, 'mima-mima');
+});
+
+ThNameGenBasics('generateName() with custom delimiter', () => {
+  const delimiter = '@';
+  const randomFunc = createFakeRandomFunc([0, 1]);
+  const generated = generateName({ randomFunc, delimiter, characterCandidates: testCharacters });
+  assert.equal(generated, 'reimu@kirisame');
+});
+
+ThNameGenBasics('generateName() with custom converter', () => {
+  const nameChunkConverter = (name, context) => name[0].toUpperCase() + name.slice(1);
+  const randomFunc = createFakeRandomFunc([0, 1]);
+  const generated = generateName({ randomFunc, characterCandidates: testCharacters, nameChunkConverter });
+  assert.equal(generated, 'Reimu-Kirisame');
+});
+
+TestInternals.run();
+ThNameGenBasics.run();
